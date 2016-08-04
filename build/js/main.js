0956a95026eb245b9c827c311d4d861ea1e90d7a
Samina Chhatwal <csaminak@gmail.com>
2016-08-04 18:19:04 -0400

    add Item function defined

--- build/js/main.js
+++ build/js/main.js
@@ -13,8 +13,24 @@
 
     ToDoController.$inject = ['toDoList'];
 
-    function ToDoController(/*toDoList*/) {
+    function ToDoController(toDoList) {
+        var that = this;
+        this.getList = toDoList.findAll();
+        this.addItem = addItem;
+        this.newItem = {};
+        this.remainingToDo = toDoList.remainingToDo();
 
+
+        /**
+         * passes an object with data into function and sends it to the
+         * toDoList service function addItem, and then sets newItem to
+         * an empty object once done, so a newItem can be added.
+         * @param   {Object}     data     contains a info about toDoItem
+         */
+        function addItem(data) {
+            toDoList.addItem(data);
+            that.newItem = {};
+        }
     }
 
 })();
@@ -25,9 +41,50 @@
     angular.module('todoApp')
         .factory('toDoList', ToDoListService);
 
+
+    /**
+     * returns an object that has functions for the service provides
+     * @return  {Object}    contains service methods
+     */
     function ToDoListService() {
-        
+        return {
+            addItem: addItem,
+            findAll: findAll
+        };
     }
+
+
+
+    /**
+     * [addItem description]
+     * @param {[type]} data [description]
+     */
+    function addItem(data) {
+        var taskList = JSON.parse(localStorage.getItem('taskList'));
+
+        if (!data) {
+            return null;
+        }
+
+        var newItem = {
+            theTask: data.theTask
+        };
+
+        taskList.push(newItem);
+        localStorage.setItem('taskList', angular.toJson(newItem));
+        return newItem;
+    }
+
+    /**
+     * [findAll description]
+     * @return {[type]} [description]
+     */
+    function findAll() {
+        try {
+            return JSON.parse(localStorage.getItem('taskList'));
+        } catch(error) {}
+    }
+
 
 })();
 
--- src/js/todo.controller.js
+++ src/js/todo.controller.js
@@ -7,7 +7,23 @@
     ToDoController.$inject = ['toDoList'];
 
     function ToDoController(toDoList) {
+        var that = this;
         this.getList = toDoList.findAll();
+        this.addItem = addItem;
+        this.newItem = {};
+        // this.remainingToDo = toDoList.remainingToDo();
+
+
+        /**
+         * passes an object with data into function and sends it to the
+         * toDoList service function addItem, and then sets newItem to
+         * an empty object once done, so a newItem can be added.
+         * @param   {Object}     data     contains a info about toDoItem
+         */
+        function addItem(data) {
+            toDoList.addItem(data);
+            that.newItem = {};
+        }
     }
 
 })();
