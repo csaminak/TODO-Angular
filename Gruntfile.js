a2c67f5c141eaa50e8c1539c731d65d7d4828dba
Samina Chhatwal <csaminak@gmail.com>
2016-08-04 15:22:52 -0400

    added all dependencies to package json and files for grunt to automate

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initconfig({
+
+        clean: ['build'],
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                ignores: ['src/js/vendor/**']
+            },
+            all: {
+                files: {
+                    src: ['src/js/**/*.js', 'test/specs/**/*.js', 'Gruntfile.js']
+                }
+            }
+        },
+
+        sass: {
+            all: {
+                files: {
+                    'build/css/styles.css': 'src/sass/main.scss'
+                }
+            }
+        },
+
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['index.html'], dest: 'build/'
+                }]
+            }
+        },
+
+        concat: {
+            js: {
+                options: {
+                    sourceMap: true,
+                },
+                src: ['src/js/*.module.js', 'src/js/*.js'],
+                dest: 'build/js/main.js'
+            }
+        },
+
+        watch: {
+            sass: {
+                files: ['src/sass/**/*.scss'],
+                tasks: ['sass']
+            },
+            js: {
+                files: ['src/js/*.js'],
+                tasks: ['jshint', 'test', 'concat']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['copy:html']
+            }
+        },
+
+        karma: {
+                all: {
+                    options: {
+                        frameworks: ['chai', 'mocha'],
+                        client: {
+                            mocha: {
+                                ui: 'tdd'
+                            }
+                        },
+                        browsers: ['PhantomJS'],
+                        singleRun: true,
+                        files: [
+                            'node_modules/angular/angular.js',
+                            'node_modules/angular-mocks/angular-mocks.js',
+                            'src/js/*.module.js',
+                            'src/js/**/*.js',
+                            'test/specs/**/*.spec.js'
+                        ]
+                    }
+                }
+            }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.registerTask('test', ['karma']);
+    grunt.registerTask('build', ['clean', 'jshint', 'test', 'concat', 'sass', 'copy']);
+    grunt.registerTask('default', ['build']);
+
+};
