953fd535b7ab2f61d8731e1bd9051f5a2de6e7ff
Samina Chhatwal <csaminak@gmail.com>
2016-08-04 17:57:37 -0400

    add findAll method which just return the todo list from local storage

--- src/js/todoList.service.js
+++ src/js/todoList.service.js
@@ -4,8 +4,27 @@
     angular.module('todoApp')
         .factory('toDoList', ToDoListService);
 
+
+    /**
+     * returns an object that has functions for the service provides
+     * @return  {Object}    contains service methods
+     */
     function ToDoListService() {
+        return {
+            addItem: addItem,
+            findAll: findAll
+        };
+    }
+
+
+    function addItem() {
         
+    }
+
+    function findAll() {
+        try {
+            return JSON.parse(localStorage.getItem('list'));
+        } catch(error) {}
     }
 
 })();
